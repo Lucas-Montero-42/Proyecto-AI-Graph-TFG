5093af2b6438a8a305d223d5f55bf29dfc0668ba
Lucas Montero Pedrosa <57368590+Lucas-Montero-42@users.noreply.github.com>
2024-10-21 12:18:14 +0200

    Mas cambios

--- Assets/BehabiorTree/BTG Nodes/Action Nodes/WaitNode.cs
+++ Assets/BehabiorTree/BTG Nodes/Action Nodes/WaitNode.cs
@@ -7,6 +7,18 @@ public class WaitNode : BTG_ActionNode
     public float duration = 1f;
     float startTime;
 
+    public int a;
+    public string b;
+    public bool c;
+    public char d;
+    public enum E
+    {
+        ab,bc,cd
+    };
+    public Vector2 f;
+    public List<int> g;
+    public GameObject h;
+
     protected override void OnStart()
     {
         startTime = Time.time;
